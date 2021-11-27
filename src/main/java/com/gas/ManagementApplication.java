1b2d89316d9fb2d7805c5bf91329041d40e87b87
longshao <54877684+gwl7944@users.noreply.github.com>
2021-11-27 12:06:45 +0800

    测试

--- src/main/java/com/gas/ManagementApplication.java
+++ src/main/java/com/gas/ManagementApplication.java
@@ -11,7 +11,17 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class ManagementApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ManagementApplication.class, args);
+        System.err.println(
+                "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t启动成功！！！\n" +
+                        "oooooooo8   o8                           o8                                                                                      o888o            o888  \n" +
+                        "888        o888oo  ooooooo   oo oooooo  o888oo   oooooooo8 oooo  oooo   ooooooo     ooooooo    ooooooooo8  oooooooo8  oooooooo8 o888oo oooo  oooo   888  \n" +
+                        " 888oooooo  888    ooooo888   888    888 888    888ooooooo  888   888 888     888 888     888 888oooooo8  888ooooooo 888ooooooo  888    888   888   888  \n" +
+                        "        888 888  888    888   888        888            888 888   888 888         888         888                 888        888 888    888   888   888  \n" +
+                        "o88oooo888   888o 88ooo88 8o o888o        888o  88oooooo88   888o88 8o  88ooo888    88ooo888    88oooo888 88oooooo88 88oooooo88 o888o    888o88 8o o888o "
+        );
+
     }
 
 }
